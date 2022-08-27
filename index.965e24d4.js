581fa6f3f39a8373539412dc7c287446d00cbe5f
VolovodenkoTaras <106447648+VolovodenkoTaras@users.noreply.github.com>
2022-08-27 09:34:40 +0000

    Deploying to gh-pages from @ VolovodenkoTaras/team5-project-html-css@e15fd459c1c97623e4a05681cadf0590cf5699c3 🚀

---
+++ index.965e24d4.js
@@ -0,0 +1,2 @@
+new Swiper(".mySwiper",{navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"}});
+//# sourceMappingURL=index.965e24d4.js.map
---
+++ index.a65bde4b.js
@@ -0,0 +1,2 @@
+new Swiper(".mySwiper",{navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"}});
+//# sourceMappingURL=index.a65bde4b.js.map
